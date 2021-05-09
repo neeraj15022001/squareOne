54556db1104a3acf674d2fdf8378701c30133811
Aman Bansal <abansal619@gmail.com>
2021-05-09 17:55:50 +0530

    Cart Added

--- api/app.js
+++ api/app.js
@@ -4,18 +4,36 @@ const port = process.env.PORT || 8000;
 var cors = require("cors");
 var firebase = require("firebase/app");
 var session = require("express-session");
-var https = require('https');
-var http = require('http')
-const fs = require('fs');
+var https = require("https");
+var http = require("http");
+const fs = require("fs");
 require("firebase/auth");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-var key = fs.readFileSync(__dirname + '/selfsigned.key', 'utf8');
-var cert = fs.readFileSync(__dirname + '/selfsigned.crt', 'utf8');
+const cartTableName = "userCart";
+
+/**
+ * Initialize Firestore configuration
+ **/
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./service_account.json");
+const { urlencoded } = require("express");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+var key = fs.readFileSync(__dirname + "/selfsigned.key", "utf8");
+var cert = fs.readFileSync(__dirname + "/selfsigned.crt", "utf8");
 var options = {
   key: key,
-  cert: cert
+  cert: cert,
 };
-var httpServer = http.createServer(app)
+var httpServer = http.createServer(app);
 var httpsServer = https.createServer(options, app);
 app.set("x-powered-by", false);
 app.use(express.urlencoded({ extended: true }));
@@ -31,25 +49,24 @@ app.get("/", (req, res) => {
     appId: "1:478348457006:web:78f2b28bfdc3ef7908dbbf",
   };
   try {
-    firebase.initializeApp(firebaseConfig)
-    res.sendStatus(200)
+    firebase.initializeApp(firebaseConfig);
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(`Error is ${error}`);
+    res.sendStatus(200);
   }
-  catch(error) {
-    console.log(`Error is ${error}`)
-    res.sendStatus(200)
-  }
-  
+
   // res.json({ status: 200 });
 });
 
 app.get("/getCurrentUser", (req, res) => {
   console.log(req.sessionID);
   var user = firebase.auth().currentUser;
-  if(user) {
+  if (user) {
     // console.log(user)
-    return res.sendStatus(200)
+    return res.sendStatus(200);
   } else {
-    return res.sendStatus(404)
+    return res.sendStatus(404);
   }
 });
 
@@ -118,9 +135,131 @@ app.get("/signOut", (req, res) => {
     });
 });
 
+app.post("/addToCart", (req, res) => {
+  const itemNameStr = req.body.itemName;
+  console.log(itemNameStr);
+  const result = addItemToDb(cartTableName, "gneeraj32595@gmail.com", itemNameStr)
+  console.log(`adding item to db ${result}`)
+  if(result)
+  {
+    res.sendStatus(200);
+  }
+  else
+  {
+    res.sendStatus(500);
+  }
+});
+
+app.post("/removeFromCart", (req, res) => {
+  const itemNameStr = req.body.itemName;
+  console.log(itemNameStr);
+  const result = removeItemFromDb(cartTableName, "gneeraj32595@gmail.com", itemNameStr);
+  console.log(`print result valur from remove ${result}`)
+  if(result)
+  {
+    res.sendStatus(200);
+  }
+  else
+  {
+    res.sendStatus(500);
+  }
+});
+
+function addItemToDb(colName, docName, itemName) {
+  console.log(`Print Check ${itemName}`);
+  const data = {
+    [itemName]: 1,
+  };
+  checkIfDocExistsInDb(colName, docName)
+    .then((res) => {
+      if (!res) 
+      {
+        console.log("Creating new entry");
+        const res = db.collection(colName).doc(docName).set(data);
+      } 
+      else 
+      {
+        console.log("Old entry increment");
+        setDbFieldCount(colName, docName, itemName, 1);
+      }
+      return 1;
+    })
+    .catch((err) => {
+      console.log(err)
+      return 0
+    });
+    return 0
+}
+
+async function removeItemFromDb(colName, docName, fieldName) {
+  let returnResult
+  await checkIfDocExistsInDb(colName, docName)
+    .then(async (res) => {
+      if (!res) 
+      {
+        console.log("No document present in DB to remove");
+        return 0
+      }
+      else 
+      {
+        // Get the `FieldValue` object
+        const FieldValue = admin.firestore.FieldValue;
+
+        // Create a document reference
+        const docRef = db.collection(colName).doc(docName);
+        const result = 0
+        // Remove the 'capital' field from the document
+        await docRef.update({
+          [fieldName]: FieldValue.delete(),
+        }).then((res) => {
+          if(res) {
+            console.log(JSON.stringify(res))
+            result = 1
+          }
+        })
+        return result
+      }
+    })
+    .then(res => {
+      console.log(`printing result from remove from database ${res}`)
+      returnResult = res
+    })
+    .catch((err) => {
+      console.log(err)
+      return 0
+    }); 
+    console.log(`value of returnResult is ${returnResult}`)
+}
+
+async function setDbFieldCount(colName, docName, fieldName, fieldCount)
+{
+  const docRef = db.collection(colName).doc(docName);
+
+  // Atomically increment the population of the city by fieldCount.
+  
+  const res = await docRef.update({
+    [fieldName]: admin.firestore.FieldValue.increment(fieldCount),
+  });
+}
+
+async function checkIfDocExistsInDb(colName, docName) {
+  const docRef = db.collection(colName).doc(docName);
+  const doc = await docRef.get();
+  if (!doc.exists) 
+  {
+    console.log("No such document!");
+    return 0;
+  } 
+  else
+  {
+    console.log("Document data:", doc.data());
+    return 1;
+  }
+}
+
 httpServer.listen(8000, () => {
-  console.log("App is now running on PORT 8000")
-})
+  console.log("App is now running on PORT 8000");
+});
 httpsServer.listen(8001, () => {
   console.log(`App is now running on port 8001}`);
 });
